6099ec8e6051bcd45cfc2d8bb2e956ae8ce0d4ec
Mexion <dev@mexion.com>
2023-08-12 19:08:43 +0200

    add posttest

--- tests/Unit/HttpLogIncomingTest.php
+++ tests/Unit/HttpLogIncomingTest.php
@@ -2,6 +2,7 @@
 
 namespace Yormy\ApiIoTracker\Tests\Unit;
 
+use Yormy\ApiIoTracker\Domain\HttpLogger\Models\LogHttpIncoming;
 use Yormy\ApiIoTracker\Tests\TestCase;
 use Yormy\ApiIoTracker\Tests\Traits\RequestTrait;
 
@@ -11,7 +12,9 @@ class HttpLogIncomingTest extends TestCase
 
     public function setUp(): void
     {
-        define('LARAVEL_START', microtime(true));
+        if (!defined('LARAVEL_START')) {
+            define('LARAVEL_START', microtime(true));
+        }
         parent::setUp();
     }
 
@@ -20,13 +23,43 @@ class HttpLogIncomingTest extends TestCase
      *
      * @group xxx
      */
-    public function Http_LogConnectionError(): void
+    public function HttpIncoming_Post_Log(): void
     {
-        $url = route('test.getroute', []);
-        $this->json('GET', $url);
-        dd();
+        LogHttpIncoming::truncate();
 
         $url = route('test.postroute', []);
         $this->json('POST', $url, ['hello' =>'ewlcome']);
+
+        $lastItem = LogHttpIncoming::latest()->first();
+        $this->assertSame($lastItem->status_code, 200);
+        $this->assertSame($lastItem->method, 'POST');
+
+        $relativeUrl = $lastItem->url;
+        $relativeUrl = str_replace('http://localhost/', '', $relativeUrl);
+        $relativeUrl = str_replace('https://localhost/', '', $relativeUrl);
+        $this->assertSame($lastItem->url, $relativeUrl);
+    }
+
+
+    /**
+     * @test
+     *
+     * @group xxx
+     */
+    public function HttpIncoming_Get_Log(): void
+    {
+        LogHttpIncoming::truncate();
+
+        $url = route('test.getroute', []);
+        $this->json('GET', $url, ['hello' =>'ewlcome']);
+
+        $lastItem = LogHttpIncoming::latest()->first();
+        $this->assertSame($lastItem->status_code, 200);
+        $this->assertSame($lastItem->method, 'GET');
+
+        $relativeUrl = $lastItem->url;
+        $relativeUrl = str_replace('http://localhost/', '', $relativeUrl);
+        $relativeUrl = str_replace('https://localhost/', '', $relativeUrl);
+        $this->assertSame($lastItem->url, $relativeUrl);
     }
 }

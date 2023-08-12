8f3f334954952188e39b92c596ccfa3279e4bbc2
Mexion <dev@mexion.com>
2023-08-12 20:22:37 +0200

    normalize

--- tests/Unit/OutgoingMaskTest.php
+++ tests/Unit/OutgoingMaskTest.php
@@ -7,48 +7,14 @@ use Yormy\ApiIoTracker\Domain\HttpLogger\Models\LogHttpOutgoing;
 use Yormy\ApiIoTracker\Tests\TestCase;
 use Yormy\StringGuard\DataObjects\UrlGuardConfig;
 
-class HttpLogMaskTest extends TestCase
+class OutgoingMaskTest extends TestCase
 {
     /**
      * @test
      *
      * @group masked
      */
-    public function HttpLog_ExcludeLogFields_Succcess(): void
-    {
-        $data = [
-            'exclude' => [
-                'headers',
-                'body',
-                'response',
-            ],
-        ];
-
-        $urlGuard = [
-            'include' => [
-                UrlGuardConfig::make('*', ['*'], $data),
-            ],
-            'exclude' => [
-            ],
-        ];
-
-        config(['api-io-tracker.url_guards' => $urlGuard]);
-
-        Http::get('https://nu.nl', ['hello' => 'kkkk']);
-        $lastItem = LogHttpOutgoing::orderBy('id', 'desc')->first();
-
-        $excludedMessage = config('api-io-tracker.excluded_message');
-        $this->assertEquals($lastItem->headers, json_encode([$excludedMessage]));
-        $this->assertEquals($lastItem->body, json_encode([$excludedMessage]));
-        $this->assertEquals($lastItem->response, $excludedMessage);
-    }
-
-    /**
-     * @test
-     *
-     * @group masked
-     */
-    public function HttpLog_MaskFields_Success(): void
+    public function Outgoing_MaskFields_Success(): void
     {
         $data = [
             'mask' => [

23beb92ca9955a31900a41f138ecf2141af15667
Mexion <dev@mexion.com>
2023-08-12 20:07:22 +0200

    incoming filter test

--- tests/Unit/IncomingIncludeExcludeTest.php
+++ tests/Unit/IncomingIncludeExcludeTest.php
@@ -13,9 +13,9 @@ class IncomingIncludeExcludeTest extends TestCase
     /**
      * @test
      *
-     * @group xxx
+     * @group incoming
      */
-    public function Incoming_Excluded_NotLogged(): void
+    public function Incoming_UrlExcluded_NotLogged(): void
     {
         $countStart = LogHttpIncoming::count();
 
@@ -40,9 +40,9 @@ class IncomingIncludeExcludeTest extends TestCase
     /**
      * @test
      *
-     * @group xxx
+     * @group incoming
      */
-    public function Incoming_NotExcluded_Logged(): void
+    public function Incoming_UrlNotExcluded_Logged(): void
     {
         $countStart = LogHttpIncoming::count();
 
@@ -60,5 +60,76 @@ class IncomingIncludeExcludeTest extends TestCase
         $this->json('POST', $url);
 
         $this->assertGreaterThan($countStart, LogHttpIncoming::count());
+    }
+
+
+    /**
+     * @test
+     *
+     * @group xxx
+     */
+    public function Incoming_UrlFieldExcluded_Excluded(): void
+    {
+        LogHttpIncoming::truncate();
+
+        $data = [
+            'exclude' => [
+                'headers',
+                'body',
+                'response',
+            ],
+        ];
+
+        $urlGuard = [
+            'include' => [
+                UrlGuardConfig::make('*', ['*'], $data),
+            ],
+            'exclude' => [
+            ],
+        ];
+        config(['api-io-tracker.incoming_url_guards' => $urlGuard]);
+
+        $url = route('test.getroute', []);
+        $this->json('GET', $url, ['hello' =>'welcome']);
+        $lastItem = LogHttpIncoming::latest()->first();
+
+        $message = config('api-io-tracker.excluded_message');
+
+        $this->assertEquals(json_encode([$message]), $lastItem->headers);
+        $this->assertEquals(json_encode([$message]), json_encode($lastItem->body));
+        $this->assertEquals($message, $lastItem->response);
+    }
+
+    /**
+     * @test
+     *
+     * @group xxx
+     */
+    public function Incoming_UrlFieldNotExcluded_Included(): void
+    {
+        LogHttpIncoming::truncate();
+
+        $data = [
+            'exclude' => [],
+        ];
+
+        $urlGuard = [
+            'include' => [
+                UrlGuardConfig::make('*', ['*'], $data),
+            ],
+            'exclude' => [
+            ],
+        ];
+        config(['api-io-tracker.incoming_url_guards' => $urlGuard]);
+
+        $url = route('test.getroute', []);
+        $this->json('GET', $url, ['hello' =>'welcome']);
+        $lastItem = LogHttpIncoming::latest()->first();
+
+        $message = config('api-io-tracker.excluded_message');
+
+        $this->assertNotEquals(json_encode([$message]), $lastItem->headers);
+        $this->assertNotEquals(json_encode([$message]), json_encode($lastItem->body));
+        $this->assertNotEquals($message, $lastItem->response);
     }
 }

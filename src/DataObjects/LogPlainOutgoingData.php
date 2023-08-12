526b9c9e7991649b1b0cef2cbe3dc869bf5d92a5
Mexion <dev@mexion.com>
2023-08-12 23:01:12 +0200

    Wip Curl

---
+++ src/DataObjects/LogPlainOutgoingData.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Yormy\ApiIoTracker\DataObjects;
+
+use Yormy\ApiIoTracker\Services\Resolvers\UserResolver;
+
+class LogPlainOutgoingData extends LogData
+{
+    public static function make(
+        string $url,
+        string $method,
+        array $headers,
+        array $params,
+        array $data
+    ): array {
+        //$headers = $request->headers();
+        $headers = self::filterHeaders($headers, $data);
+
+        $body = $params;
+        $body = self::filterBody($body, $data);
+
+        $excludedMessage = config('api-io-tracker.excluded_message');
+        if (isset($data['EXCLUDE'])) {
+            if (in_array('RESPONSE', $data['EXCLUDE'])) {
+                $response = $excludedMessage;
+            }
+        }
+
+        $data = [
+            'url' => $url,
+            'method' => $method,
+            'headers' => substr(json_encode($headers),0, 200),
+            'body' => substr(json_encode($body), 0, 6000),
+            'response' => '???',//$response ? substr($response, 0, 6000) : null,
+        ];
+
+        $user = UserResolver::getCurrent();
+        if ($user) {
+            $data['user_id'] = $user->id;
+            $data['user_type'] = get_class($user);
+        }
+
+        return $data;
+    }
+
+    protected static function getGlobalFilter(): array
+    {
+        return static::upperCase(config('api-io-tracker.field_masking.outgoing'));
+    }
+}
---
+++ src/Observers/Listeners/PlainRequestListener.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Yormy\ApiIoTracker\Observers\Listeners;
+
+use Yormy\ApiIoTracker\DataObjects\LogPlainOutgoingData;
+use Yormy\ApiIoTracker\Models\LogHttpOutgoing;
+use Yormy\StringGuard\Services\UrlGuard;
+
+class PlainRequestListener
+{
+    public function handle(object $event): void
+    {
+        if (!config('api-io-tracker.enabled_outgoing')) {
+            $this->include = false;
+            $this->data = [];
+            return;
+        }
+
+        $url = $event->url;
+        $method = $event->method;
+        $config = config('api-io-tracker.outgoing_url_guards');
+        $include = UrlGuard::isIncluded($url, $method, $config);
+        $data = UrlGuard::getData($url, $method, $config);
+
+//        if (! $this->include) {
+//            return;
+//        }
+
+       $logData = LogPlainOutgoingData::make(
+            $url,
+            $method,
+            $event->headers,
+            $event->params,
+//           $event->request,
+//           $event?->response,
+           $data
+       );
+
+        LogHttpOutgoing::create([
+            'status' => 'OK',
+            ...$logData,
+        ]);
+    }
+}
